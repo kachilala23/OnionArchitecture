eb7fd174a5e4043e9c6a0eba5a31b968a7694edc
onyekachila <onyekachila@gmail.com>
2020-05-22 15:17:12 +0100

    Initial Commit With Onion Architecture Set Up

---
+++ CustomerApp.Core/DomainService/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using CustomerApp.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerApp.Core.DomainService
+{
+    public interface ICustomerRepository
+    {
+        // CRUD for customers
+        //Create
+        Customer Create(Customer customer);
+        // Read
+        Customer ReadById(int id);
+        List<Customer> ReadAll();
+        // Update
+        Customer Update(Customer customer);
+        // Delete
+        Customer Delete(int id)
+    }
+}
---
+++ CustomerApp.Infratructure.Static.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerApp.Infratructure.Static.Data.Repositories
+{
+    class CustomerRepository
+    {
+    }
+}
